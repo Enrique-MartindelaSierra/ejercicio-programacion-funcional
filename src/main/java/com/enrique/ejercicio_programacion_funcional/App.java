de3bf39897557a8611cc61fbbe4328aa467d634a
Enrique-MartindelaSierra <enriquemdlse22@gmail.com>
2023-03-14 22:42:07 +0100

    Initial commit

---
+++ src/main/java/com/enrique/ejercicio_programacion_funcional/App.java
@@ -0,0 +1,13 @@
+package com.enrique.ejercicio_programacion_funcional;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
